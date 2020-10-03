3f53aaa802e6d82bf38912d2393b0b344fd42fd2
NgocHa0108 <phamngocha0108@gmail.com>
2020-10-03 21:02:37 +0700

    ha

---
+++ blog/app/Http/Controllers/StudentController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+use DB;
+
+class StudentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $students= Student::all();//lay het
+        //$student = Student::orderBy('id', 'desc')->get();
+        return view('layout.admin_student', ['studentList' => $students]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        dd('Students Controller create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Student $student)
+
+    {
+       
+        //neu chi truyen vao $student -.nhan duoc id cua student
+        $studentObjEloquentModel = Student::find($student);
+        $studentObjQueryBuilder = DB::table('students')->find($student);
+       
+
+        
+
+        //neu truyen vao Student $student -> thuc hien truy van tim Student co id = $student
+        $studentObj = $student;
+
+         return view('students.detail_student', ['studentObj' => $studentObj]);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Student $student)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Student $student)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Student  $student
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Student $student)
+    {
+        //
+    }
+}
